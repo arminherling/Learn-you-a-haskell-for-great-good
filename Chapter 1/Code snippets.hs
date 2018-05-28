a894bf11228e0eeec76965a924bca5250de2aa01
Armin Herling <armin.herling@gmail.com>
2018-05-28 22:55:00 +0200

    Functions, ranges and list comprehensions

---
+++ Chapter 1/Code snippets.hs
@@ -0,0 +1,22 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100
+                        then x
+                        else x * 2
+
+boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x ]
+sequence50To100Remainder3 = [ x | x <- [50..100], x `mod` 7==3 ]
+sequenceNot13Not15Not19 = [ x | x <- [10..20], x /= 13, x /= 15, x /= 19 ]
+combingTwoLists = [ x+y | x <- [1,2,3], y <- [10,100,1000] ]
+combingTwoLists' = [ x*y | x <- [2,5,10], y <- [8,10,11] ]
+combingTwoLists'' = [ x*y | x <- [2,5,10], y <- [8,10,11], x*y > 50 ]
+
+nouns = ["hobo", "frog", "pope"]
+adjectives = ["lazy", "grouchy", "scheming"]
+wordCombinations = [adjective ++ " " ++ noun | adjective <- adjectives, noun <- nouns]
+
+lenght' xs = sum [ 1 | _ <- xs]
+
+removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z'] ]
+nestedLists = [[1,3,5,2,3,1,2,4,5], [1,2,3,4,5,6,7,8,9], [1,2,4,2,1,6,3,1,3,2,3,6]]
+evenNumbersInNestedLists xxs = [ [ x | x <- xs, even x] | xs <- xxs ]
