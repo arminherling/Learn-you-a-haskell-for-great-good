6e05926bffdb67929eef2d3e764010621be5582d
Armin Herling <armin.herling@gmail.com>
2018-05-29 21:22:14 +0200

    Calculating BMI with guards

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -43,3 +43,10 @@ bmiTell bmi
     | bmi <= 25.0 = "Looking good!"
     | bmi <= 30.0 = "You're overweight. Let's work out together!"
     | otherwise = "You're obese. Go see a doctor."
+
+bmiTell' :: Double -> Double -> String
+bmiTell' weight height  -- height is meters, e.g. 1.75
+    | weight / height ^ 2 <= 18.5 = "You're underweight, eat more!"
+    | weight / height ^ 2 <= 25.0 = "Looking good!"
+    | weight / height ^ 2 <= 30.0 = "You're overweight. Let's work out together!"
+    | otherwise = "You're obese. Go see a doctor."
