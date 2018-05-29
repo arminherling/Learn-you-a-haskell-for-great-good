4401073063cb1b710b9136d45c3e8c71b9f7b3be
Armin Herling <armin.herling@gmail.com>
2018-05-29 20:37:45 +0200

    More pattern matching

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -1,3 +1,9 @@
 lucky :: Int -> String
 lucky 7 = "LUCKY NUMBER SEVEN!"
 lucky x = "Sorry, you're out of luck, pal!"
+
+sayMe :: Int -> String
+sayMe 1 = "One!"
+sayMe 2 = "Two!"
+sayMe 3 = "Three!"
+sayMe x = "Not between 1 and 3"
