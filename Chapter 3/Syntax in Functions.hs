995588aab71264deb41e0bbf63cb9101bc107416
Armin Herling <armin.herling@gmail.com>
2018-05-29 20:43:40 +0200

    Pattern matching with arrays

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -26,3 +26,9 @@ second (_, y, _) = y
 
 third :: (a, b, c) -> c
 third (_, _, z) = z
+
+tell :: (Show a) => [a] -> String
+tell [] = "The list is empty"
+tell (x:[]) = "The list has one element: " ++ show x
+tell (x:y:[]) = "The list has two elements: " ++ show x ++ " and " ++ show y
+tell (x:y:_) = "This list is long. The first two elements are: " ++ show x ++ " and " ++ show y
