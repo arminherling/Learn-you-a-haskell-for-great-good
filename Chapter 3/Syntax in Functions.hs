546c932b5105929381717f6c99bbb36420fb5856
Armin Herling <armin.herling@gmail.com>
2018-05-30 22:21:29 +0200

    BMI function with guards and where

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -62,3 +62,10 @@ a `myCompare` b
     | a <= b    = LT
     | otherwise = GT
     
+bmiTell'' :: Double -> Double -> String
+bmiTell'' weight height
+    | bmi <= 18.5 = "You're underweight, eat more!"
+    | bmi <= 25.0 = "Looking good!"
+    | bmi <= 30.0 = "You're overweight. Let's work out together!"
+    | otherwise = "You're obese. Go see a doctor."
+    where bmi = weight / height ^ 2
