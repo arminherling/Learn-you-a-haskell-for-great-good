9c861f64d5ee14d69f18daa82c3a7a9246a28930
Armin Herling <armin.herling@gmail.com>
2018-06-01 22:27:19 +0200

    Let in list comprehension

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -107,3 +107,6 @@ cylinder r h =
     let sideArea = 2 * pi * r * h
         topArea = pi * r ^ 2
     in sideArea + 2 * topArea
+
+calcBmis' :: [(Double, Double)] -> [Double]
+calcBmis' xs = [ bmi | (w, h) <- xs, let bmi = w / h ^ 2 ]
