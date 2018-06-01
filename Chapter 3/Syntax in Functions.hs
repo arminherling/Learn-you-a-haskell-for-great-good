6a294d4706a328578624f69e24dd9737c157f561
Armin Herling <armin.herling@gmail.com>
2018-06-01 23:52:29 +0200

    Case as where in function

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -124,3 +124,9 @@ describeList ls = "The list is " ++ case ls of [] -> "empty."
                                                [x] -> "a singleton list."
                                                xs -> "a long list."
 
+                                               
+describeList' :: [a] -> String
+describeList' ls = "The list is " ++ what ls
+    where what [] = "empty."
+          what [x] = "a singleton list."
+          what xs = "a longer list."
