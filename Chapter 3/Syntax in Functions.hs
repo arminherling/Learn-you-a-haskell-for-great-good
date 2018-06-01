6aebd51928ecfb9aadc5e698333c95018442f691
Armin Herling <armin.herling@gmail.com>
2018-06-01 22:37:03 +0200

    Let and pattern matching in list comprehension

--- Chapter 3/Syntax in Functions.hs
+++ Chapter 3/Syntax in Functions.hs
@@ -110,3 +110,6 @@ cylinder r h =
 
 calcBmis' :: [(Double, Double)] -> [Double]
 calcBmis' xs = [ bmi | (w, h) <- xs, let bmi = w / h ^ 2 ]
+
+calcBmis'' :: [(Double, Double)] -> [Double]
+calcBmis'' xs = [ bmi | (w, h) <- xs, let bmi = w / h ^ 2, bmi > 25.0]
