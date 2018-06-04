a54b33f12eee63962991ab4714ffde1978e0ddca
Armin Herling <armin.herling@gmail.com>
2018-06-04 23:50:58 +0200

    Elem with right fold

--- Chapter 5/Higher order functions.hs
+++ Chapter 5/Higher order functions.hs
@@ -67,3 +67,6 @@ map'' f xs = foldr (\x acc -> f x : acc) [] xs
 
 map''' :: (a -> b) -> [a] -> [b]
 map''' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
