713b1eede191d7138990decdc6bbff887199446c
Armin Herling <armin.herling@gmail.com>
2018-06-04 22:50:45 +0200

    Folds with foldl

--- Chapter 5/Higher order functions.hs
+++ Chapter 5/Higher order functions.hs
@@ -58,3 +58,6 @@ addThree' = \x -> \y -> \z -> x + y + z
 
 flip''' :: (a -> b -> c) -> b -> a -> c
 flip''' f = \x y -> f y x
+
+sum' :: (Num a) => [a] -> a
+sum' xs = foldl (\acc x -> acc + x) 0 xs
