3bd24c0790442c8c81385bff0046249e47f49957
Armin Herling <armin.herling@gmail.com>
2018-06-05 00:06:02 +0200

    Reverse with fold left

--- Chapter 5/Higher order functions.hs
+++ Chapter 5/Higher order functions.hs
@@ -70,3 +70,9 @@ map''' f xs = foldl (\acc x -> acc ++ [f x]) [] xs
 
 elem' :: (Eq a) => a -> [a] -> Bool
 elem' y ys = foldr (\x acc -> if x == y then True else acc) False ys
+
+maximum' :: (Ord a) => [a] -> a
+maximum' = foldl1 max
+
+reverse' :: [a] -> [a]
+reverse' = foldl (\acc x -> x : acc) []
