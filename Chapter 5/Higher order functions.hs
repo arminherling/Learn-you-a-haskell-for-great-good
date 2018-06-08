53d69e8116f2ffee0170afe96973a467dec12d8b
Armin Herling <armin.herling@gmail.com>
2018-06-09 00:09:55 +0200

    Point-free style

--- Chapter 5/Higher order functions.hs
+++ Chapter 5/Higher order functions.hs
@@ -106,3 +106,6 @@ negativeSumOfListTail' = map (negate . sum . tail) [[1..5],[3..6],[1..7]]
 
 replicateProduct = replicate 2 (product (map (*3) (zipWith max [1,2] [4,5])))
 replicateProduct' = replicate 2 . product . map (*3) $ zipWith max [1,2] [4,5]
+
+someFunction' x = ceiling (negate (tan (cos (max 50 x))))
+someFunction'' = ceiling . negate . tan . cos . max 50 -- point-free style
