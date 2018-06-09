72bbde7bae42e1f669939dda50177cb43cd8e979
Armin Herling <armin.herling@gmail.com>
2018-06-10 00:36:24 +0200

    Function for checking if a list is a substring of another list

--- Chapter 6/Importing Modules.hs
+++ Chapter 6/Importing Modules.hs
@@ -5,3 +5,7 @@ numUniques = length . nub
 
 wordNums :: String -> [(String,Int)]
 wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
+
+-- same as isInfixOf in Data.List
+isIn :: (Eq a) => [a] -> [a] -> Bool
+needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
