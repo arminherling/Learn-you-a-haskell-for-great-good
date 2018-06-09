0a1233614d3a6811116e4ef9bc5c5477c6d45a9e
Armin Herling <armin.herling@gmail.com>
2018-06-10 00:05:46 +0200

    Counting words in a list

--- Chapter 6/Importing Modules.hs
+++ Chapter 6/Importing Modules.hs
@@ -2,3 +2,6 @@ import Data.List
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
+
+wordNums :: String -> [(String,Int)]
+wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
