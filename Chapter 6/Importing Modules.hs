9cc5b2ab66ad3b17e20fec0036e9f314860a6387
Armin Herling <armin.herling@gmail.com>
2018-06-11 21:10:39 +0200

    Caesar cipher

--- Chapter 6/Importing Modules.hs
+++ Chapter 6/Importing Modules.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Char
 
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
@@ -9,3 +10,12 @@ wordNums = map (\ws -> (head ws, length ws)) . group . sort . words
 -- same as isInfixOf in Data.List
 isIn :: (Eq a) => [a] -> [a] -> Bool
 needle `isIn` haystack = any (needle `isPrefixOf`) (tails haystack)
+
+stringToListOfNumbers :: [Char] -> [Int]
+stringToListOfNumbers xs = map ord xs
+
+encode :: Int -> String -> String
+encode offset msg = map (\c -> chr $ ord c + offset) msg
+
+decode :: Int -> String -> String
+decode shift msg = encode (negate shift) msg
