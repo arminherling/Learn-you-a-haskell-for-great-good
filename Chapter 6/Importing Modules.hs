b780ac8059bc3b43307bb084ffa344891adbfe0f
Armin Herling <armin.herling@gmail.com>
2018-06-13 21:59:57 +0200

    Association list or dictionary

--- Chapter 6/Importing Modules.hs
+++ Chapter 6/Importing Modules.hs
@@ -34,3 +34,23 @@ firstTo40 = find (\x -> digitSum x == 40) [1..]
 
 firstTo :: Int -> Maybe Int
 firstTo n = find (\x -> digitSum x == n) [1..]
+
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")]
+
+findKey :: (Eq k) => k -> [(k, v)] -> v
+findKey key xs = snd . head . filter (\(k, v) -> key == k) $ xs
+
+findKey' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey' key [] = Nothing
+findKey' key ((k,v):xs)
+    | key == k  = Just v
+    | otherwise = findKey' key xs
+
+findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
+findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
