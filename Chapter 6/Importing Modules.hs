2fa2f9a7b80bdff109882f5d7ff3a1fcbc0f28df
Armin Herling <armin.herling@gmail.com>
2018-06-14 22:14:34 +0200

    Transforming association lists into maps and inserting new value pairs

--- Chapter 6/Importing Modules.hs
+++ Chapter 6/Importing Modules.hs
@@ -1,6 +1,9 @@
 import Data.List
 import Data.Char
 
+import qualified Data.Map as Map
+
+
 numUniques :: (Eq a) => [a] -> Int
 numUniques = length . nub
 
@@ -54,3 +57,14 @@ findKey' key ((k,v):xs)
 
 findKey'' :: (Eq k) => k -> [(k, v)] -> Maybe v
 findKey'' key xs = foldr (\(k, v) acc -> if key == k then Just v else acc) Nothing xs
+
+phoneBook' :: Map.Map String String
+phoneBook' = Map.fromList $
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")]
+
+newBook = Map.insert "grace" "341-9021" phoneBook'
