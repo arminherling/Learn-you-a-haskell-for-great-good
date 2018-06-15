8e141ef39c297ceb275464ec94dfab5e8e6a5596
Armin Herling <armin.herling@gmail.com>
2018-06-15 21:53:42 +0200

    Rect area

--- Chapter 6/Geometry.hs
+++ Chapter 6/Geometry.hs
@@ -12,3 +12,6 @@ sphereVolume radius = (4.0 / 3.0) * pi * (radius ^ 3)
 
 sphereArea :: Float -> Float
 sphereArea radius = 4 * pi * (radius ^ 2)
+
+rectArea :: Float -> Float -> Float
+rectArea a b = a * b
