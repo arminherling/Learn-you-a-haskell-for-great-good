bc37de467b126c6edd829e974fbe548bfb6cdf2c
Armin Herling <armin.herling@gmail.com>
2018-06-15 21:56:28 +0200

    Cuboid volume

--- Chapter 6/Geometry.hs
+++ Chapter 6/Geometry.hs
@@ -16,5 +16,8 @@ sphereArea radius = 4 * pi * (radius ^ 2)
 cuboidArea :: Float -> Float -> Float -> Float
 cuboidArea a b c = rectArea a b * 2 + rectArea a c * 2 + rectArea c b * 2
 
+cuboidVolume :: Float -> Float -> Float -> Float
+cuboidVolume a b c = rectArea a b * c
+
 rectArea :: Float -> Float -> Float
 rectArea a b = a * b
