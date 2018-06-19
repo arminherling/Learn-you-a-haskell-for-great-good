ef6bf800772e9ebde728ccc231e62fe6873de374
Armin Herling <armin.herling@gmail.com>
2018-06-19 23:01:01 +0200

    Partially applied constructors

--- Chapter 7/Our own types.hs
+++ Chapter 7/Our own types.hs
@@ -4,3 +4,5 @@ data Shape = Circle Float Float Float | Rectangle Float Float Float Float
 area :: Shape -> Float
 area (Circle _ _ r) = pi * r ^ 2
 area (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+
+differentSizedCircles = map (Circle 10 20) [10,20..50]
