add4f072d239e91f877dc2544e92d6b2e52f01db
Armin Herling <armin.herling@gmail.com>
2018-06-20 22:14:23 +0200

    Function for creating a base circle

--- Chapter 7/Our own types.hs
+++ Chapter 7/Our own types.hs
@@ -12,4 +12,7 @@ nudge :: Shape -> Float -> Float -> Shape
 nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))
 
+baseCircle :: Float -> Shape
+baseCircle r = Circle (Point 0 0) r
+
 differentSizedCircles = map (Circle (Point 10 20)) [10,20..50]
