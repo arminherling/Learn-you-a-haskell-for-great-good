16755e5c1f637c3a21b2bc073e94fe4a51608263
Armin Herling <armin.herling@gmail.com>
2018-06-24 20:47:03 +0200

    Added dot product for the vector

--- Chapter 7/Vector.hs
+++ Chapter 7/Vector.hs
@@ -3,3 +3,5 @@ data Vector a = Vector a a a deriving (Show)
 vplus :: (Num a) => Vector a -> Vector a -> Vector a 
 (Vector i j k) `vplus` (Vector l m n) = Vector (i+l) (j+m) (k+n)
 
+dotProd :: (Num a) => Vector a -> Vector a -> a
+(Vector i j k) `dotProd` (Vector l m n) = i*l + j*m + k*n
