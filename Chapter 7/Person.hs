e98f118aaa43341ac38b0f8dc6d28755aa2dd296
Armin Herling <armin.herling@gmail.com>
2018-06-27 20:48:41 +0200

    Reading string into types

--- Chapter 7/Person.hs
+++ Chapter 7/Person.hs
@@ -6,3 +6,9 @@ data Person = Person { firstName :: String
 mikeD = Person {firstName = "Michael", lastName = "Diamond", age = 43}
 adRock = Person {firstName = "Adam", lastName = "Horovitz", age = 41}
 mca = Person {firstName = "Adam", lastName = "Yauch", age = 44}
+
+mysteryDude = "Person { firstName =\"Michael\"" ++
+                     ", lastName = \"Diamond\"" ++
+                     ", age = 43}"
+
+mysteryDudeAsPerson = read mysteryDude :: Person
