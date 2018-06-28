5746f2be1f7bd7cd151507f36510c8058816d220
Armin Herling <armin.herling@gmail.com>
2018-06-28 19:54:49 +0200

    Type synonyms

--- Chapter 7/Our own types.hs
+++ Chapter 7/Our own types.hs
@@ -55,3 +55,20 @@ data Day = Monday | Tuesday | Wednesday | Thursday | Friday | Saturday | Sunday
 
 minDay = minBound :: Day
 maxDay = maxBound :: Day
+
+type PhoneNumber = String
+type Name = String
+type PhoneBook = [(Name, PhoneNumber)]
+
+phoneBook :: [(String, String)]
+phoneBook =
+    [("betty", "555-2938")
+    ,("bonnie", "452-2928")
+    ,("patsy", "493-2928")
+    ,("lucille", "205-2928")
+    ,("wendy", "939-8282")
+    ,("penny", "853-2492")
+    ]
+
+inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
+inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
