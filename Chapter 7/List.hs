9869fee73a4ecc79c66d9b7b1c00024549ac8e42
Armin Herling <armin.herling@gmail.com>
2018-06-30 19:15:05 +0200

    Added an infix for the list

--- Chapter 7/List.hs
+++ Chapter 7/List.hs
@@ -1,3 +1,4 @@
-data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
 -- same as 
 -- data List a = Empty | Cons { listHead :: a, listTail :: List a } deriving (Show, Read, Eq, Ord)
