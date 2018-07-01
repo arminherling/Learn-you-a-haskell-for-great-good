49493a7efcf9919c946e899081fa5a6220772cf2
Armin Herling <armin.herling@gmail.com>
2018-07-01 22:05:57 +0200

    Adding lists together

--- Chapter 7/List.hs
+++ Chapter 7/List.hs
@@ -2,3 +2,8 @@ infixr 5 :-:
 data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
 -- same as 
 -- data List a = Empty | Cons { listHead :: a, listTail :: List a } deriving (Show, Read, Eq, Ord)
+
+infixr 5 ^++
+(^++) :: List a -> List a -> List a
+Empty ^++ ys = ys
+(x :-: xs) ^++ ys = x :-: (xs ^++ ys)
