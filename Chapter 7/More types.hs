3d463318b1f37c56b7b92239b3fe35d0b49dd232
Armin Herling <armin.herling@gmail.com>
2018-07-06 21:16:46 +0200

    Instance of Maybe definition

--- Chapter 7/More types.hs
+++ Chapter 7/More types.hs
@@ -10,3 +10,9 @@ instance Show TrafficLight where
     show Red = "Red light"
     show Green = "Green light"
     show Yellow = "Yellow light"
+
+-- example of instance of Maybe is defined
+-- instance (Eq m) => Eq (Maybe m) where
+--     Just x == Just y = x == y
+--     Nothing == Nothing = True
+--     _ == _ = False
