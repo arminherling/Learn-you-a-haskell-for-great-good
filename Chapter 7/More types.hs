8042419aec74af7f1770aadc1bea280ac259914f
Armin Herling <armin.herling@gmail.com>
2018-07-07 23:12:17 +0200

    Yes no type class

--- Chapter 7/More types.hs
+++ Chapter 7/More types.hs
@@ -16,3 +16,25 @@ instance Show TrafficLight where
 --     Just x == Just y = x == y
 --     Nothing == Nothing = True
 --     _ == _ = False
+
+class YesNo a where
+    yesno :: a -> Bool
+
+instance YesNo Int where
+    yesno 0 = False
+    yesno _ = True
+
+instance YesNo [a] where
+    yesno [] = False
+    yesno _ = True
+
+instance YesNo Bool where
+    yesno = id
+
+instance YesNo (Maybe a) where
+    yesno (Just _) = True
+    yesno Nothing = False
+
+instance YesNo TrafficLight where
+    yesno Red = False
+    yesno _ = True
