ce8d63704e8b0ebe1b051bdb28d8bf114e63bcd0
Armin Herling <armin.herling@gmail.com>
2018-07-09 22:05:48 +0200

    Functor of either

--- Chapter 7/Functors.hs
+++ Chapter 7/Functors.hs
@@ -7,3 +7,7 @@ instance Functor' [] where
 instance Functor' Maybe where
     fmap f (Just x) = Just (f x)
     fmap f Nothing = Nothing
+
+instance Functor' (Either a) where
+    fmap f (Right x) = Right (f x)
+    fmap f (Left x) = Left x
